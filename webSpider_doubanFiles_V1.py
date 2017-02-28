6263c1b879f06697835fe30fd2d20ff29b6ceb5f
v1siuol <1098741925@qq.com>
2017-02-27 21:14:51 -0500

    Create webSpider_doubanFiles_V1.py

---
+++ webSpider_doubanFiles_V1.py
@@ -0,0 +1,31 @@
+"""
+豆瓣爬虫V1:返回排行榜电影名字
+V2:将会接入api提取json分析每部电影的细节
+Introduction: This program is output the title-lists of top ten latest movie charts to a csv file
+__author__ = v1siuol
+__date__ = 2017.2.27
+"""
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import csv
+
+
+def main():
+    print("Begin----------------------------------")
+    html = urlopen("https://movie.douban.com/chart")
+    bs0bj = BeautifulSoup(html, "lxml")
+    lstMovies = bs0bj.find("div", {"class": "indent"}).findAll("a", {"class": "nbg"})
+    csvFile = open("./lstMoviesTitles.csv", "w+")
+    try:
+        writer = csv.writer(csvFile)
+        writer.writerow(["豆瓣新片榜"])
+        for i in lstMovies:
+            writer.writerow([i['title']])
+    finally:
+        csvFile.close()
+    print("------------------------------------End")
+
+# -------------test case-----------------
+if __name__ == "__main__":
+    if False:
+        main()
